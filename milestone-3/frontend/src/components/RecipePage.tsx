fa26d734ae6f91215cc2261c9e28d1dc184514ba
rishabb02 <67344869+rishabb02@users.noreply.github.com>
2023-01-04 14:30:14 -0800

    finished milestone 3 and 3.5

--- milestone-3/frontend/src/components/RecipePage.tsx
+++ milestone-3/frontend/src/components/RecipePage.tsx
@@ -1,10 +1,11 @@
 import recipes from "../recipeData";
 import { useParams } from "react-router-dom";
-import { useState, useEffect, ChangeEvent } from 'react'
+import React, { useState, useEffect, ChangeEvent, ReactComponentElement } from 'react'
+import { findDOMNode } from "react-dom";
 
 interface Recipe {
     name: string;
-    description: string;
+    desc: string;
     image: string;
     ingredients: string[];
     instructions: string[];
@@ -15,32 +16,111 @@ interface Recipe {
 export default function RecipePage() {
     const { id } = useParams();
 
-    const [recipe, setRecipe] = useState<Recipe>()
+    const [recipe, setRecipe] = useState<Recipe[]>([])
     const [ingredient, setIngredient] = useState("")
+    const [instruction, setInstruction] = useState("")
+    const [allIngredients, setAllIngredients] = useState<string[]>([]);
+    const [allInstructions, setAllInstructions] = useState<string[]>([]);
 
     useEffect(() => {
-        const currentRecipe = recipes.find(recipe => recipe.name === id?.replace("-", " ").toUpperCase())
-        // setRecipe(currentRecipe)
+        const findRecipe = recipes.find(recipe => {
+            const title = id!.replace("-", " ")
+            const words = title?.split(" ")
+
+            console.log("words : ", words)
+
+            let recipeTitle = words?.map((word) => {
+                return word[0].toUpperCase() + word.substring(1)
+            }).join(" ")
+
+            return recipe.name === recipeTitle;
+        });
+
+        console.log("findRecipe: ", findRecipe)
+
+        setRecipe((currentRecipe) => [...currentRecipe, findRecipe!]);
+        setAllIngredients(findRecipe!.ingredients)
+
+        setAllInstructions(findRecipe!.instructions)
     }, [])
     // const recipe: Recipe | undefined = recipes.find(recipe => recipe.id === id);
     return (
         <div>
-            <h1>
-                {recipe?.name}
-            </h1>
-            <form>
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", paddingLeft: "30px", paddingRight: "30px" }}>
+                <div>
+                    {
+                        recipe.map((ele, idx) => {
+                            return (
+                                <div>
+                                    <h1>{ele.name}</h1>
+                                    <p>{ele.desc}</p>
+                                    <img src={ele.image} alt="Pani-Puri" />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div >
+                    <div>
+                        <h3 className="ingredient">Ingredients</h3>
+                        <ul className="">
+                            {
+                                allIngredients.map((ingred, idx) =>
+                                    <React.Fragment key={idx}>
+                                        <li>{ingred}</li>
+                                    </React.Fragment>
+                                )
+                            }
+                        </ul>
+                    </div>
+                    <div className="form-ingredient">
+                        <input
+                            placeholder=""
+                            value={ingredient} // add newIngredient as the input's value
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                // this event handler updates the value of newIngredient
+                                setIngredient(e.target.value);
+                            }}
+                        />
+                        <button onClick={() => {
+                            setAllIngredients([...allIngredients, ingredient])
+                            setIngredient("")
+                        }}>
+                            Add Ingredient
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+
+            <div>
+                <h3 className="instruction">Instructions</h3>
+                <ol className="instructions">
+                    {
+                        allInstructions.map((ingred, idx) =>
+                            <React.Fragment key={idx}>
+                                <li>{ingred}</li>
+                            </React.Fragment>
+                        )
+                    }
+                </ol>
+            </div>
+            <div>
                 <input
-                    placeholder="2 cups of spinach"
-                    value={ingredient} // add newIngredient as the input's value
+                    placeholder=""
+                    value={instruction} // add newIngredient as the input's value
                     onChange={(e: ChangeEvent<HTMLInputElement>) => {
                         // this event handler updates the value of newIngredient
-                        setIngredient(e.target.value);
+                        setInstruction(e.target.value);
                     }}
                 />
-                <button onClick={() => setIngredient([...allIngredients, ingredient])}>
-                    Add Ingredient
+                <button onClick={() => {
+                    setAllInstructions([...allInstructions, instruction])
+                    setInstruction("")
+                }}>
+                    Add Instruction
                 </button>
-            </form>
+            </div>
         </div>
     )
 }
